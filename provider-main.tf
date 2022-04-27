23fd5c34b837ed060274f1566cd772c2d8375fb1
Mesut Doganguzel <mesutguzel3501@gmail.com>
2022-04-27 02:39:47 -0400

    provider updated

--- provider-main.tf
+++ provider-main.tf
@@ -12,9 +12,9 @@ terraform {
   }
 }
 
-# provider "aws" {
-#   access_key = var.aws_access_key
-#   secret_key = var.aws_secret_key
-#   region     = var.aws_region
-# }
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = var.aws_region
+}
 
