e2fa5844d5885abb2b6b71ff35f848e10c4c371e
Mesut Doganguzel <mesutguzel3501@gmail.com>
2022-04-27 03:08:21 -0400

    provider updated

--- provider-main.tf
+++ provider-main.tf
@@ -13,8 +13,8 @@ terraform {
 }
 
 provider "aws" {
-  access_key = var.AWS_ACCESS_KEY
-  secret_key = var.AWS_SECRET_KEY
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
   region     = var.AWS_DEFAULT_REGION
 }
 
