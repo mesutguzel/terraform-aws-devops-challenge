35def02be159080a485ac0f4c870e6616f84301e
Mesut Doganguzel <77083678+mesutguzel@users.noreply.github.com>
2022-05-07 02:13:20 -0400

    Delete docker-task directory

--- docker-task/run.py
+++
@@ -1,5 +0,0 @@
-from market import app
-import os
-#Checks if the run.py file has executed directly and not imported
-if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000, debug=os.environ.get('DEBUG')=='1')
