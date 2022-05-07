6f0f57e0c76465564c253404ee07c1e3becab285
Mesut Doganguzel <77083678+mesutguzel@users.noreply.github.com>
2022-05-07 02:18:58 -0400

    Update run.py

--- docker-task/run.py
+++ docker-task/run.py
@@ -2,4 +2,4 @@ from market import app
 import os
 #Checks if the run.py file has executed directly and not imported
 if __name__ == '__main__':
-    app.run(host='0.0.0.0',port=5000, debug=os.environ.get('DEBUG')=='1')
+    app.run(host='0.0.0.0',port=5000)
